affd41a58d144afc5478dc4199e728f0f7362654
miskadlugosova <miskadlugosova@gmail.com>
2020-03-14 19:52:34 +0100

    algorithm for graphs with 20 vertices checking if there is a solution for colouring where every path has a length smaller than 5

--- main.cpp
+++ main.cpp
@@ -2,15 +2,19 @@
 #include <fstream>
 #include <vector>
 #include <set>
+#include <unistd.h>
 
 using namespace std;
 
 ofstream result;
 
-vector<vector<pair<int, int> > > graph;   //graph contains of n rows (each for every vertex), each row contains 3 elements in vector of pairs  - neighbouring vector and the colour of the edge
+vector<vector<pair<int, int> > >
+        graph;   //graph contains of n rows (each for every vertex), each row contains 3 elements in vector of pairs  - neighbouring vector and the colour of the edge
 vector<int> red; // colour == 0
 vector<int> blue; //colour == 1
 set<vector<int>> sety;
+bool existsopt = false;
+bool correctfile = true;
 
 int finddif(int i) {   //returns the different coloured edge
     if (graph[i][0].second != graph[i][1].second && graph[i][0].second != graph[i][2].second)
@@ -49,21 +53,16 @@ void evaluate() {
     }
 
     if (correct) {
-        /*for (int i = 0; i < red.size(); i++) {
-            if (i != 0) result << " " << red[i] / 2;
-            else result << red[i] / 2;
-        }
-        result << endl;
-        result << endl;*/
+
+
         sety.insert(red);
 
-        for (int i = 0; i < graph.size(); i++) {
-            for (int j = 0; j < 3; j++) {
-                cout << graph[i][j].second;
-            }
-            cout << endl;
-        }
-        cout << endl;
+        bool tmp = true;                         //ci kazdy graf ma mnozinu cuest kde najdlhsia je dlzky max 5
+         for (int i = 6; i < red.size(); i++) {
+             if (red[i] != 0) tmp = false;
+         }
+         if (tmp) existsopt = true;
+
     }
 
     for (int i = 0; i < red.size(); i++) {                   //vynuluje pocty dlzok ciest
@@ -98,26 +97,9 @@ bool existspath(int from, int vertex1, int vertex2, int colour) {
 void alg(int vertex, int edge) {
 
 
-    if (graph[vertex][edge].second != -1) {
-        if (edge < 2) {
-            alg(vertex, edge + 1);
-        } else if (vertex < graph.size() - 1) {
-            alg(vertex + 1, 0);
-        } else if (vertex == graph.size() - 1 && edge == 2) {     //ak sme ofarbili poslendu hranu
-            evaluate();
-        }
-    }    //check if the edge is "blank"
-    else {
+    if (!existsopt) {
 
-        if (!existspath(vertex, vertex, graph[vertex][edge].first, 1) &&
-            possibleedge(vertex, 1) && possibleedge(graph[vertex][edge].first,
-                                                    1)) { //neexistuje cyklus ak tu hranu ofarbim a mozem ofarbit (je to ok aj pre ten druhy vrchol
-            graph[vertex][edge].second = 1;
-
-            for (int i = 0; i < 3; i++) {      //ofarbenie aj z druheho vrcholu do vertex
-                if (graph[graph[vertex][edge].first][i].first == vertex) graph[graph[vertex][edge].first][i].second = 1;
-            }
-
+        if (graph[vertex][edge].second != -1) {
             if (edge < 2) {
                 alg(vertex, edge + 1);
             } else if (vertex < graph.size() - 1) {
@@ -125,79 +107,142 @@ void alg(int vertex, int edge) {
             } else if (vertex == graph.size() - 1 && edge == 2) {     //ak sme ofarbili poslendu hranu
                 evaluate();
             }
-        }
+        }    //check if the edge is "blank"
+        else {
 
-        if (!existspath(vertex, vertex, graph[vertex][edge].first, 0) &&
-            possibleedge(vertex, 0) &&
-            possibleedge(graph[vertex][edge].first, 0)) { //neexistuje cyklus ak tu hranu ofarbim a mozem ofarbit
-            graph[vertex][edge].second = 0;
+            if (!existspath(vertex, vertex, graph[vertex][edge].first, 1) &&
+                possibleedge(vertex, 1) && possibleedge(graph[vertex][edge].first,
+                                                        1)) { //neexistuje cyklus ak tu hranu ofarbim a mozem ofarbit (je to ok aj pre ten druhy vrchol
+                graph[vertex][edge].second = 1;
 
-            for (int i = 0; i < 3; i++) {
-                if (graph[graph[vertex][edge].first][i].first == vertex) graph[graph[vertex][edge].first][i].second = 0;
+                for (int i = 0; i < 3; i++) {      //ofarbenie aj z druheho vrcholu do vertex
+                    if (graph[graph[vertex][edge].first][i].first == vertex)
+                        graph[graph[vertex][edge].first][i].second = 1;
+                }
+
+                if (edge < 2) {
+                    alg(vertex, edge + 1);
+                } else if (vertex < graph.size() - 1) {
+                    alg(vertex + 1, 0);
+                } else if (vertex == graph.size() - 1 && edge == 2) {     //ak sme ofarbili poslendu hranu
+                    evaluate();
+                }
             }
 
-            if (edge < 2) alg(vertex, edge + 1);
-            else if (vertex < graph.size() - 1) alg(vertex + 1, 0);
+            if (!existspath(vertex, vertex, graph[vertex][edge].first, 0) &&
+                possibleedge(vertex, 0) &&
+                possibleedge(graph[vertex][edge].first, 0)) { //neexistuje cyklus ak tu hranu ofarbim a mozem ofarbit
+                graph[vertex][edge].second = 0;
 
-            else if (vertex == graph.size() - 1 && edge == 2) {     //ak sme ofarbili poslendu hranu
-                evaluate();
+                for (int i = 0; i < 3; i++) {
+                    if (graph[graph[vertex][edge].first][i].first == vertex)
+                        graph[graph[vertex][edge].first][i].second = 0;
+                }
+
+                if (edge < 2) alg(vertex, edge + 1);
+                else if (vertex < graph.size() - 1) alg(vertex + 1, 0);
+
+                else if (vertex == graph.size() - 1 && edge == 2) {     //ak sme ofarbili poslendu hranu
+                    evaluate();
+                }
             }
-        }
-        graph[vertex][edge].second = -1;
-        for (int i = 0; i < 3; i++) {      //ofarbenie aj z druheho vrcholu do vertex
-            if (graph[graph[vertex][edge].first][i].first == vertex) graph[graph[vertex][edge].first][i].second = -1;
+            graph[vertex][edge].second = -1;
+            for (int i = 0; i < 3; i++) {      //ofarbenie aj z druheho vrcholu do vertex
+                if (graph[graph[vertex][edge].first][i].first == vertex)
+                    graph[graph[vertex][edge].first][i].second = -1;
+            }
         }
     }
+
 }
 
 
 int main() {
     int numofver;
+    long int l;
+    string s;
     pair<int, int> p;
     ifstream myfile;
-    result.open("16g3e_res.txt");
-    myfile.open("/home/jerry/CLionProjects/rpgrafy/16g3e");
+    result.open("20g3eres");
+    myfile.open("/home/jerry/CLionProjects/rpgrafy/20g3e.txt");
     myfile >> numofver;
     if (numofver * 3 / 2 % 2 == 1) result << "graf by neobsahoval parny pocet hran" << endl;
+
     else {
-        int order = 1;
+        //int order = 0;
         while (!myfile.eof()) {
             int a;
-            result << "I am processing graph number " << order << endl;
-            order++;
-            for (int i = 0; i < numofver; i++) {
+            //cout << "I am processing graph number " << order << endl;
+            //order++;
+            existsopt = false;
+
+            myfile >> s >> l >> s;
+            cout << "Graph " << l << endl;
+            for (int i = 0; i < numofver; i++) {      // nacita vstup
                 red.push_back(0);                    //najdlhsia cesta moze byt dlzky numofver-1
                 blue.push_back(0);
-
-                vector<pair<int, int>> tmp;
-                for (int j = 0; j < 3; j++) {
+                vector<pair<int, int>>
+                        tmp;
+                for (int j = 0; j < 3; j++) {        // tri susedne vrcholy pre kazdy vektor
                     myfile >> a;
                     pair<int, int> p;
-                    p.first = a;
+                    p.first = a - 1;                   //v 20-vrcholovych su cislovane od 1 nie od 0
                     p.second = -1;
                     tmp.push_back(p);
                 }
                 graph.push_back(tmp);
             }
 
+            if (myfile.eof()) continue;             //po precitani posledneho grafu neregistruje eof ale nacitava posledne cislo znovu niekolkokrat
+            alg(0, 0);                 //spusti vypocet na danom grafe
 
-            alg(0, 0);
-            for (int i = 0; i < graph.size(); i++) {
-                graph[i].clear();
-            }
-            graph.clear();
-            red.clear();
-            blue.clear();
-
-            for (vector <int> v : sety) {                        //zapisem rozne moznosti ciest do result
+            /*result << "Graph " << l << endl;
+            for (vector<int> v : sety) {                        //zapisem rozne moznosti ciest do result
                 for (int i = 0; i < v.size(); i++) {
                     if (i != 0) result << " " << v[i] / 2;
                     else result << v[i] / 2;
                 }
                 result << endl;
                 result << endl;
+            }*/
+
+
+            if (!existsopt) {            // pre pocet vrcholov 16 ci existuje mnozina ciest kde najdlhsia je max dlzky 5
+                correctfile = false;
+                result << "I am processing graph number " << l << endl;
+                for (vector<int> v : sety) {                        //zapisem rozne moznosti ciest do result
+                    for (int i = 0; i < v.size(); i++) {
+                        if (i != 0) result << " " << v[i] / 2;
+                        else result << v[i] / 2;
+                    }
+                    result << endl;
+                    result << endl;
+                }
             }
+
+
+            for (int i = 0; i < graph.size(); i++) {
+                graph[i].clear();
+            }
+            graph.clear();
+            red.clear();
+            blue.clear();
+            sety.clear();
+
+
+
+            /* for (vector<int> v : sety) {                        //zapisem rozne moznosti ciest do result
+                 for (int i = 0; i < v.size(); i++) {
+                     if (i != 0) result << " " << v[i] / 2;
+                     else result << v[i] / 2;
+                 }
+                 result << endl;
+                 result << endl;
+             }*/
         }
+
+        if (correctfile) result << "Graphs were correct" << endl;
+        else result << "Some of the graphs were incorrect" << endl;
     }
 
     cout << "skoncil som" << endl;
